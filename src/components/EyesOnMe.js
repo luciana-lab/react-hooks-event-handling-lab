1d680e5cc754fa4facf67628e8fbf7daf9e00af9
Luciana Sugeta <lulusugeta@gmail.com>
2021-11-10 10:59:11 -0500

    commit tree f155a883e6d0fb33afecd50f2633d060fb3d28e1

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+const EyesOnMe = () => {
+    return (
+        <div>
+            <button
+                onFocus={() => console.log("Good!")}
+                onBlur={() => console.log("Hey! Eyes on me!")}
+            >
+                Eyes on me
+            </button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+const Keypad = () => {
+    return (
+        <div>
+            <input onChange={() => console.log("Entering password...")} type="password" />
+        </div>
+    )
+}
+
+export default Keypad;
